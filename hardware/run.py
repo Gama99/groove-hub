885ab35d29661dd4a4b252d551914166ef30c377
Gama99 <70278732+Gama99@users.noreply.github.com>
2024-05-05 01:43:03 -0400

    Frogot to add run.py to previous commit

--- hardware/run.py
+++ hardware/run.py
@@ -21,7 +21,6 @@ def main():
                             print("Waiting for write permission...")
                             id, _ = check_for_card()
                             ## remeber to update write_ready with the response of svelte
-                            time.sleep(1) # Reduce CPU usage
                         if id:  # Still the same card and permission granted
                             print("Writing new Spotify link to the card...")
                             # Add your code here to write to the card
@@ -33,12 +32,10 @@ def main():
                     print("Waiting for write permission...")
                     id, current_text = check_for_card()
                     ## remeber to update write_ready with the response of svelte
-                    time.sleep(1) # Reduce CPU usage
                 if id:  # Still the same card and permission granted
                     print("Writing new Spotify link to the card...")
                     # Add your code here to write to the card
             this_id = id # Update this_id to the last detected card
-            time.sleep(1) # Reduce CPU usage
     except KeyboardInterrupt:
         GPIO.cleanup()
         raise    
